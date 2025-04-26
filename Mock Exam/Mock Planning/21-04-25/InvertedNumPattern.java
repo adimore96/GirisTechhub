6ee84a64ddae1d68437edb3bf8e79f2e2f7826c0
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-04-27 00:38:38 +0530

    Inverted Number Pattern program

---
+++ Mock Exam/Mock Planning/21-04-25/InvertedNumPattern.java
@@ -0,0 +1,24 @@
+/*
+19. Print Inverted Number Pattern
+Description: Write a program to print an inverted number pattern.
+Input: n = 5
+Output:
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+*/
+
+public class InvertedNumPattern{
+    public static void main(String args[]){
+        int num = 5;
+
+        for(int i=num; i>=1; i--){
+            for(int j=i; j>=1; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
