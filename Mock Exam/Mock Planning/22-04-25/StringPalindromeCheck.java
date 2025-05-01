477833e74460336feef63f1a72c1767950263352
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-05-01 23:57:47 +0530

    String Palindrome Check

---
+++ Mock Exam/Mock Planning/22-04-25/StringPalindromeCheck.java
@@ -0,0 +1,27 @@
+/*
+21. Check if a String is Palindrome
+Description: Check if the given string is a palindrome.
+
+Input: "madam"
+Output: Palindrome
+*/
+
+public class StringPalindromeCheck{
+    public static void main(String args[]){
+        String str = new String("madam");
+        boolean isPalindrome = true;
+        
+        for(int i=0, j=str.length()-1; i<str.length()/2; i++, j--){
+            if(str.charAt(i) != str.charAt(j)){
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if(isPalindrome){
+            System.out.println(str+" is a Palindrome String..!");
+        }else{
+            System.out.println(str+" is not a Palindrome String..!");
+        }
+    }
+}
