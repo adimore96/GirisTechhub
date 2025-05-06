88443ea3bb12be3bc7a581cb02bbf4ef62470d07
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-05-06 18:33:21 +0530

    Number Pyramid Pattern

--- Mock Exam/Mock Planning/21-04-25/NumberPyramidPattern.java
+++ Mock Exam/Mock Planning/21-04-25/NumberPyramidPattern.java
@@ -22,10 +22,14 @@ public class NumberPyramidPattern{
             }
 
             //
+            // int j=1;
             for(int j=1; j<=i; j++){
                 System.out.print(j);
             }
-            
+
+            for(int j = i-1; j>=1; j--){
+                System.out.print(j);
+            }
             System.out.println();
         }
     }
