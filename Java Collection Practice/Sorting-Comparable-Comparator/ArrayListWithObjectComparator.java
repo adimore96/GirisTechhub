9c59a7e8e77b50dc9979ef606e825697379e6233
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-05-07 00:58:20 +0530

    Sort object with multiple fields using Comparator Interface

---
+++ Java Collection Practice/Sorting-Comparable-Comparator/ArrayListWithObjectComparator.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Pojo{
+    private int id;
+    private String name;
+
+    Pojo(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    // public int compare(Object obj)
+}
+
+ class SortById implements Comparator<Pojo>{
+    // override
+    public int compare(Pojo obj1, Pojo obj2){
+        if(obj1.getId()>obj1.getId()){
+            return 1;
+        }
+        else if(obj1.getId() < obj2.getId()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+ }
+ class SortByName implements Comparator<Pojo>{
+    // override
+    public int compare(Pojo obj1, Pojo obj2){
+        return obj1.getName().compareTo(obj2.getName());
+    }
+ }
+
+public class ArrayListWithObjectComparator{
+    public static void main(String args[]){
+        ArrayList<Pojo> al = new ArrayList<>();
+
+        al.add(new Pojo(10,"ABC"));
+        al.add(new Pojo(5,"XYZ"));
+        al.add(new Pojo(15,"PQR"));
+
+        SortById sid = new SortById();
+        System.out.println("Sort by ID");
+        Collections.sort(al,sid);
+
+        ListIterator<Pojo> itr = al.listIterator();
+
+        while(itr.hasNext()){
+            Pojo obj = itr.next();
+            System.out.println(obj.getId()+"    "+obj.getName());
+        }
+
+
+        SortByName sname = new SortByName();
+        System.out.println("Sort by Name");
+        Collections.sort(al,sname);
+
+         itr = al.listIterator();
+
+        while(itr.hasNext()){
+            Pojo obj = itr.next();
+            System.out.println(obj.getId()+"    "+obj.getName());
+        }
+    }
+}
